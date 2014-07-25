dc4168e07a2d9be0beba06dfdf9b1dee9fede9bb
Zhou Wei <zhouwei6160@gmail.com>
2014-07-25 10:36:56 +0800

    DLL添加了基本的参数合法性检查，有些比如输入XML的合法性还可以进一步深入检查的

--- CCBelock/CCBelock.cpp
+++ CCBelock/CCBelock.cpp
@@ -3,9 +3,15 @@
 
 #include "stdafx.h"
 #include "CCBelock.h"
+using namespace std;
 
 CCBELOCK_API long Open(long lTimeOut)
 {
+	assert(lTimeOut>0);
+	if (lTimeOut<=0)
+	{
+		return ELOCK_ERROR_PARAMINVALID;
+	}
 	return ELOCK_ERROR_SUCCESS;
 }
 
@@ -16,17 +22,31 @@ CCBELOCK_API long Close()
 
 CCBELOCK_API long Notify(const char *pszMsg)
 {
+	assert(NULL!=pszMsg && strlen(pszMsg)>0);
+	if (NULL==pszMsg || strlen(pszMsg)==0)
+	{
+		return ELOCK_ERROR_PARAMINVALID;
+	}
 	return ELOCK_ERROR_SUCCESS;
 }
 
 void cdecl myRecvMsgRotine(const char *pszMsg)
 {
-
+	assert(NULL!=pszMsg && strlen(pszMsg)>0);
+	if (NULL==pszMsg || strlen(pszMsg)==0)
+	{
+		return;
+	}
 }
 
 typedef void (cdecl *RecvMsgRotine)(const char *pszMsg);
 
 CCBELOCK_API int SetRecvMsgRotine(RecvMsgRotine pRecvMsgFun)
 {
+	assert(NULL!=pRecvMsgFun);
+	if (NULL==pRecvMsgFun)
+	{
+		return ELOCK_ERROR_PARAMINVALID;
+	}
 	return ELOCK_ERROR_SUCCESS;
 }
