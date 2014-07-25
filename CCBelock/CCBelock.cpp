30ee9f041916208ea47a2ef14380fde51e1d0397
Zhou Wei <zhouwei6160@gmail.com>
2014-07-25 10:48:06 +0800

    两个配置参数放到单独的名字空间zwCfg里面

--- CCBelock/CCBelock.cpp
+++ CCBelock/CCBelock.cpp
@@ -4,15 +4,18 @@
 #include "stdafx.h"
 #include "CCBelock.h"
 using namespace std;
-const long JC_CCBDLL_TIMEOUT=3600;	//最长超时时间为1个小时，更长也没有意义了
-const int	JC_MSG_MAXLEN=128*1024;	//最长为下位机RAM的大小，更大也没有意义了
+
+namespace zwCfg{
+	const long	JC_CCBDLL_TIMEOUT=3600;	//最长超时时间为1个小时，更长也没有意义了
+	const int	JC_MSG_MAXLEN=128*1024;	//最长为下位机RAM的大小，更大也没有意义了
+}
 
 
 CCBELOCK_API long Open(long lTimeOut)
 {
 	//必须大于0，小于JC_CCBDLL_TIMEOUT，限制在一个合理范围内
-	assert(lTimeOut>0 && lTimeOut<JC_CCBDLL_TIMEOUT);
-	if (lTimeOut<=0 || lTimeOut>=JC_CCBDLL_TIMEOUT)
+	assert(lTimeOut>0 && lTimeOut<zwCfg::JC_CCBDLL_TIMEOUT);
+	if (lTimeOut<=0 || lTimeOut>=zwCfg::JC_CCBDLL_TIMEOUT)
 	{
 		return ELOCK_ERROR_PARAMINVALID;
 	}
@@ -26,10 +29,11 @@ CCBELOCK_API long Close()
 
 CCBELOCK_API long Notify(const char *pszMsg)
 {
+	//输入必须有内容，但是最大不得长于下位机内存大小，做合理限制
 	assert(NULL!=pszMsg);
 	int inlen=strlen(pszMsg);
-	assert(inlen>0 && inlen<JC_MSG_MAXLEN);
-	if (NULL==pszMsg || inlen==0 || inlen>=JC_MSG_MAXLEN )
+	assert(inlen>0 && inlen<zwCfg::JC_MSG_MAXLEN);
+	if (NULL==pszMsg || inlen==0 || inlen>=zwCfg::JC_MSG_MAXLEN )
 	{
 		return ELOCK_ERROR_PARAMINVALID;
 	}
@@ -38,10 +42,11 @@ CCBELOCK_API long Notify(const char *pszMsg)
 
 void cdecl myRecvMsgRotine(const char *pszMsg)
 {
+	//输入必须有内容，但是最大不得长于下位机内存大小，做合理限制
 	assert(NULL!=pszMsg);
 	int inlen=strlen(pszMsg);
-	assert(inlen>0 && inlen<JC_MSG_MAXLEN);
-	if (NULL==pszMsg || inlen==0 || inlen>=JC_MSG_MAXLEN )
+	assert(inlen>0 && inlen<zwCfg::JC_MSG_MAXLEN);
+	if (NULL==pszMsg || inlen==0 || inlen>=zwCfg::JC_MSG_MAXLEN )
 	{
 		return;
 	}
