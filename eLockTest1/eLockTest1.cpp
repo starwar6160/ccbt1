7f72e1f4ebea1ad57ff378f113204a3b6142afc1
Zhou Wei <zhouwei6160@gmail.com>
2014-07-25 11:00:08 +0800

    做了Open的测试代码

--- eLockTest1/eLockTest1.cpp
+++ eLockTest1/eLockTest1.cpp
@@ -4,31 +4,20 @@
 #include "stdafx.h"
 #include "CCBelock.h"
 
-#define _DEBUG722
-#ifdef _DEBUG722
-int Foo(int a, int b)
+
+TEST(ccbElockTest,OpenTest)
 {
-	if (a == 0 || b == 0)
-	{
-		throw "don't do that";
-	}
-	int c = a % b;
-	if (c == 0)
-		return b;
-	return Foo(b, c);
+	EXPECT_EQ(ELOCK_ERROR_SUCCESS,Open(60));
+#ifdef NDEBUG
+	EXPECT_EQ(ELOCK_ERROR_PARAMINVALID,Open(0));
+	EXPECT_EQ(ELOCK_ERROR_PARAMINVALID,Open(-30));
+	EXPECT_EQ(ELOCK_ERROR_PARAMINVALID,Open(4000));
+#endif // NDEBUG
 }
 
-TEST(FooTest, HandleNoneZeroInput)
+TEST(ccbElockDeathTest,OpenTest)
 {
-	EXPECT_EQ(2, Foo(4, 10));
-	EXPECT_EQ(6, Foo(30, 18));
+	EXPECT_DEBUG_DEATH(Open(0),"");
+	EXPECT_DEBUG_DEATH(Open(-30),"");
+	EXPECT_DEBUG_DEATH(Open(4000),"");
 }
-#endif // _DEBUG722
-
-
-//int _tmain(int argc, _TCHAR* argv[])
-//{
-//	int opr=Open(500);
-//	return 0;
-//}
-
