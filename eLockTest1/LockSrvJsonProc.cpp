7a2c0518d655984f18da4a1fe1590e4a2b487557
Zhou Wei <zhouwei6160@gmail.com>
2014-07-31 17:26:38 +0800

    emuSrv内部处理流程正在分解过程中

--- eLockTest1/LockSrvJsonProc.cpp
+++ eLockTest1/LockSrvJsonProc.cpp
@@ -2,12 +2,12 @@
 #include "CCBelock.h"
 using namespace boost::property_tree;
 //把锁具服务器端处理Json的函数集中于此，便于单元测试
-void myLockActive(const JC_MSG_TYPE type, ptree &pt );
 
 const JC_MSG_TYPE lockParseJson( const string & inJson, ptree &pt );
-
 void LockOutJson( ptree pt, string &outJson );
+void myLockActive(const JC_MSG_TYPE type, ptree &pt );
 
+//此函数没有实际功能，功能都在下级子函数中，便于单元测试；此函数在此是满足联网的单元测试的；
 int zwjclms_command_proc(const string &inJson,string &outJson)
 {
 	//	CCB 1.1版本算法ECIES(椭圆曲线集成加密公钥算法)安全初始化演示开始
@@ -23,21 +23,15 @@ int zwjclms_command_proc(const string &inJson,string &outJson)
 	}
 	assert(inJson.length()>9);	//json最基本的符号起码好像要9个字符左右
 	ptree pt;
-
+	//解析输入JSON，并对其处理，结果在pt中，类型返回在mtype中
 	JC_MSG_TYPE mtype=lockParseJson(inJson, pt);
-
-	switch (mtype)
-	{
-	case JCMSG_LOCK_ACTIVE_REQUEST:
-		myLockActive(mtype,pt);
-		break;
-	}
-
+	//把pt输出为json返回
 	LockOutJson(pt, outJson);
 
 	return mtype;
 }
 
+//锁具激活消息的具体处理函数。
 void myLockActive(const JC_MSG_TYPE type, ptree &pt )
 {
 	pt.put("LockMan","BeiJing JinChu");
@@ -45,6 +39,7 @@ void myLockActive(const JC_MSG_TYPE type, ptree &pt )
 	pt.put("LockPubKey","BJpnccGKE5muLO3RLOe+hDjUftMJJwpmnuxEir0P3ss5/sxpEKNQ5AXcSsW1CbC/pXlqAk9/NZoquFJXHW3n1Cw=,");
 }
 
+//进来的Json,对其分类，然后内容解析到pt中，并根据分类调用相应的底层函数进行组织相应的返回值的处理存入pt中，返回分类
 const JC_MSG_TYPE lockParseJson( const string & inJson, ptree &pt )
 {
 	JC_MSG_TYPE mtype=JCMSG_INVALID_TYPE;
@@ -59,9 +54,16 @@ const JC_MSG_TYPE lockParseJson( const string & inJson, ptree &pt )
 		mtype=JCMSG_LOCK_ACTIVE_REQUEST;
 		myLockActive(mtype,pt);
 	}
+	switch (mtype)
+	{
+	case JCMSG_LOCK_ACTIVE_REQUEST:
+		myLockActive(mtype,pt);
+		break;
+	}
 	return mtype;
 }
 
+//把处理完毕的pt中的内容输出为Json输出
 void LockOutJson( ptree pt, string &outJson )
 {
 	std::stringstream ss2;
