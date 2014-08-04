c99f37bd92d64caeb3a6e18e8d4f12b067ab4eef
Zhou Wei <zhouwei6160@gmail.com>
2014-08-04 15:05:51 +0800

    新增了几个常量字符串定义，集中定义JSON命令字符串，以便减少不一致的问题

---
+++ CCBelock/jcJsonCommandDefine.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "zwCcbElockHdr.h"
+
+namespace jcAtmcConvertDLL{
+	//JSONÃüÁî×Ö·û´®
+	const char *JCSTR_LOCK_ACTIVE_REQUEST="Lock_Secret_Key";
+	const char *JCSTR_LOCK_INIT="Lock_System_Init";
+	const char *JCSTR_READ_CLOSECODE="Lock_Close_Code_ATM";
+}
