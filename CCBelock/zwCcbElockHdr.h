2d6ea62351a247725f55c642005b3414bde87dd6
Zhou Wei <zhouwei6160@gmail.com>
2014-08-08 15:16:33 +0800

    回退的修订集: c089cdb81f39

--- CCBelock/zwCcbElockHdr.h
+++ CCBelock/zwCcbElockHdr.h
@@ -58,8 +58,11 @@ enum{
 };
 
 namespace zwccbthr{
-void wait(int milliseconds);
+	void wait(int milliseconds);
+void zwStartLockCommThread(void);	//启动与锁具之间的通讯线程
+void zwStopLockCommThread(void);	//停止与锁具之间的通讯线程
 const string getString(void);
+
 }	//namespace zwccbthr{
 
 typedef void (cdecl *RecvMsgRotine)(const char *pszMsg);
