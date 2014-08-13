5b0ed32843eec8c3c10d674a9c030d3f0d3c8c5c
Zhou Wei <zhouwei6160@gmail.com>
2014-08-13 22:56:20 +0800

    DLL的DllMain添加了日志，记录到jinchu2014.log文件

--- CCBelock/dllmain.cpp
+++ CCBelock/dllmain.cpp
@@ -1,33 +1,40 @@
 // dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "stdafx.h"
 
-int PocoLogTestMain(void)  ;
+Poco::LogStream *g_log=NULL;
+int PocoLogInit(void)  ;
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
 					 )
 {
+	PocoLogInit();
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
 		OutputDebugStringA("JinChu.CCB.Elock.DLL Load 20140813.2123");
+		g_log->notice("JINCHU ELOCK DLL_PROCESS_ATTACH");
+		break;
 	case DLL_THREAD_ATTACH:
+		g_log->notice("JINCHU ELOCK DLL_THREAD_ATTACH");
+		break;
 	case DLL_THREAD_DETACH:
+		g_log->notice("JINCHU ELOCK DLL_THREAD_DETACH");
+		break;
 	case DLL_PROCESS_DETACH:
+		g_log->notice("JINCHU ELOCK DLL_PROCESS_DETACH");
 		break;
 	}
-	PocoLogTestMain();
 	return TRUE;
 }
 
-int PocoLogTestMain(void)  
+int PocoLogInit(void)  
 {  
-
 	Poco::AutoPtr<Poco::Channel> channel;  
 	{  
 		Poco::AutoPtr<Poco::Channel> fileChannel(new Poco::FileChannel());  
-		fileChannel->setProperty("path", "sample813.log");  
+		fileChannel->setProperty("path", "jinchu2014.log");  
 		fileChannel->setProperty("archive", "timestamp");  
 		fileChannel->setProperty("compress", "true");  
 
@@ -38,11 +45,8 @@ int PocoLogTestMain(void)
 
 
 	Poco::Logger::root().setChannel(channel.get());  
-	Poco::Logger& logger = Poco::Logger::get("LoggerTest");  
-	Poco::LogStream ls(logger);  
-	ls.trace() << "This is a trace." << std::endl;  
-	ls.warning() << "This is a warning." << std::endl;  
-	ls.error() << "This is a error错误消息" << std::endl;  
+	Poco::Logger& logger = Poco::Logger::get("JinChuElock");  
+	g_log=new Poco::LogStream(logger); 
 	return 0;  
 }  
 
