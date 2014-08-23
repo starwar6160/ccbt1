283c3b0ecbffc7e5462d37cba610b800f708b551
Zhou Wei <zhouwei6160@gmail.com>
2014-08-23 22:14:46 +0800

    时间同步代码分离到单独代码文件了

--- eLockTest1/ReadCloseCode0004Test.cpp
+++ eLockTest1/ReadCloseCode0004Test.cpp
@@ -41,3 +41,58 @@ TEST_F(ccbElockTest,ReadCloseCodeTest0004)
 }
 #endif // _DEBUG_READ_CLOSE_CODE
 
+#ifdef _DEBUG_RECV_INIT_CLOSECODE
+//接收锁具主动发送的初始闭锁码测试
+TEST_F(ccbElockTest,RecvInitClose1000)
+{
+	EXPECT_EQ(ELOCK_ERROR_SUCCESS,SetRecvMsgRotine(myATMCRecvMsgRotine));	
+	const JC_MSG_TYPE msgType=JCMSG_SEND_INITCLOSECODE;	//设定消息类型
+	//ATMC生成XML消息
+	string strLockActiveXML;	//容纳生成的消息XML
+	//具体生成消息XML
+	ptree pt;
+	jcAtmcMsg::zwAtmcTestMsgGen(msgType,strLockActiveXML, pt);	
+
+	Notify(strLockActiveXML.c_str());
+
+	while (s_repInitCloseCodeXML.length()==0)
+	{
+		Sleep(200);
+	}
+	//Sleep(ZW_END_WAIT);
+	EXPECT_LT(42,s_repInitCloseCodeXML.length());
+	string ccbop,ccbname;
+	zwGetCCBMsgType(s_repInitCloseCodeXML,ccbop,ccbname);
+	EXPECT_EQ("1000",ccbop);
+	EXPECT_EQ("SendShutLockCode",ccbname);
+}
+#endif // _DEBUG_RECV_INIT_CLOSECODE
+
+
+#ifdef _DEBUG_RECV_VERIFY_CODE
+//接收锁具主动发送的验证码测试
+TEST_F(ccbElockTest,RecvVerifyClose1002)
+{
+	EXPECT_EQ(ELOCK_ERROR_SUCCESS,SetRecvMsgRotine(myATMCRecvMsgRotine));	
+	const JC_MSG_TYPE msgType=JCMSG_SEND_UNLOCK_CERTCODE;	//设定消息类型
+	//ATMC生成XML消息
+	string strLockActiveXML;	//容纳生成的消息XML
+	//具体生成消息XML
+	ptree pt;
+	jcAtmcMsg::zwAtmcTestMsgGen(msgType,strLockActiveXML, pt);	
+
+	Notify(strLockActiveXML.c_str());
+
+	while (s_repVerifyCodeXML.length()==0)
+	{
+		Sleep(200);
+	}
+	//Sleep(ZW_END_WAIT);
+	EXPECT_LT(42,s_repVerifyCodeXML.length());
+	string ccbop,ccbname;
+	zwGetCCBMsgType(s_repVerifyCodeXML,ccbop,ccbname);
+	EXPECT_EQ("1002",ccbop);
+	EXPECT_EQ("SendUnLockIdent",ccbname);
+}
+#endif // _DEBUG_RECV_VERIFY_CODE
+
---
+++ eLockTest1/TimeSyncTest.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "jcElockTestHdr.h"
+#include "CCBelock.h"
+
+#ifdef _DEBUG_TIMESYNC
+TEST_F(ccbElockTest,TimeSyncTest0003)
+{
+	EXPECT_EQ(ELOCK_ERROR_SUCCESS,SetRecvMsgRotine(myATMCRecvMsgRotine));	
+	const JC_MSG_TYPE msgType=JCMSG_TIME_SYNC;	//设定消息类型
+	//ATMC生成XML消息
+	string strSendLockActInfoXML;	//容纳生成的消息XML
+	//具体生成消息XML
+	ptree pt;
+	jcAtmcMsg::zwAtmcTestMsgGen(msgType,strSendLockActInfoXML, pt);	
+
+	if (ELOCK_ERROR_SUCCESS==m_connStatus)
+	{
+		EXPECT_EQ(ELOCK_ERROR_SUCCESS,Notify(strSendLockActInfoXML.c_str()));	
+		Sleep(90*1000);
+		EXPECT_EQ(ELOCK_ERROR_SUCCESS,Notify(strSendLockActInfoXML.c_str()));	
+	}
+	else
+	{
+		EXPECT_EQ(ELOCK_ERROR_CONNECTLOST,Notify(strSendLockActInfoXML.c_str()));		
+		cout<<"Server not Start!"<<endl;
+	}
+	Sleep(1*1000);
+}
+#endif // _DEBUG_TIMESYNC
+
