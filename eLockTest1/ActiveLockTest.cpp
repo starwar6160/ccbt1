958ed3072ae724c6d058503ff00f74a8b491cfc1
Zhou Wei <zhouwei6160@gmail.com>
2014-08-23 22:09:38 +0800

    激活锁具的0000和0001测试一起放到一个单独的代码文件中了，因为有逻辑联系；

---
+++ eLockTest1/ActiveLockTest.cpp
@@ -0,0 +1,99 @@
+#include "stdafx.h"
+#include "jcElockTestHdr.h"
+#include "CCBelock.h"
+
+#ifdef _DEBUG_ACTREQ
+//锁具激活请求报文的在线测试
+TEST_F(ccbElockTest,LockActiveTest0000)
+{
+	printf("%d HEX=%s\n",67386086,zwCode8ToHex(67386086).c_str());
+	printf("%s HEX=%016X\n","67386086","67386086");
+
+	//Open(25);
+	EXPECT_EQ(ELOCK_ERROR_SUCCESS,SetRecvMsgRotine(myATMCRecvMsgRotine));	
+	const JC_MSG_TYPE msgType=JCMSG_LOCK_ACTIVE_REQUEST;	//设定消息类型
+	//ATMC生成XML消息
+	string strLockActiveXML;	//容纳生成的消息XML
+	//具体生成消息XML
+	ptree pt;
+	jcAtmcMsg::zwAtmcTestMsgGen(msgType,strLockActiveXML, pt);	
+	//strLockActiveXML=
+	//"<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+	//"<root><TransCode>0000</TransCode>"
+	//"<TransName>CallForActInfo</TransName>"
+	//"<TransDate>20140807</TransDate>"
+	//"<TransTime>153235</TransTime>"
+	//"<DevCode>12345698</DevCode>"
+	//"<SpareString1>NULL</SpareString1>"
+	//"<SpareString2>NULL</SpareString2></root>";
+	//"<?xml version=\"1.0\" encoding=\"utf-8\"?><root><TransCode>0000</TransCode><TransName>CallForActInfo</TransName><TransDate>20140807</TransDate><TransTime>173828</TransTime><DevCode>12345698Z</DevCode><SpareString1>NULL</SpareString1><SpareString2>NULL</SpareString2></root>";
+	//下面一行是一整行原生XML，方便网页测试使用，没有转义字符
+	// <?xml version="1.0" encoding="utf-8"?><root><TransCode>0000</TransCode><TransName>CallForActInfo</TransName><TransDate>20140807</TransDate><TransTime>173828</TransTime><DevCode>12345698Z</DevCode><SpareString1>NULL</SpareString1><SpareString2>NULL</SpareString2></root>
+	//cout<<"Sleep 65 seconds,please wait"<<endl;
+
+	if (ELOCK_ERROR_SUCCESS==m_connStatus)
+	{
+		EXPECT_EQ(ELOCK_ERROR_SUCCESS,Notify(strLockActiveXML.c_str()));		
+	}
+	else
+	{
+		EXPECT_EQ(ELOCK_ERROR_CONNECTLOST,Notify(strLockActiveXML.c_str()));		
+		cout<<"Server not Start!"<<endl;
+	}
+
+	while (s_repActReqXML.length()==0)
+	{
+		Sleep(200);
+	}
+
+	Sleep(ZW_END_WAIT);
+	EXPECT_LT(42,s_repActReqXML.length());
+
+	string ccbop,ccbname;
+	zwGetCCBMsgType(s_repActReqXML,ccbop,ccbname);
+	EXPECT_EQ("0000",ccbop);
+	EXPECT_EQ("CallForActInfo",ccbname);
+	//Sleep(6*1000);
+}
+#endif // _DEBUG_ACTREQ
+
+#ifdef _DEBUG_SEND_ACTINFO
+//锁具发送激活信息(PSK)报文(初始化)的在线测试
+TEST_F(ccbElockTest,LockSendActInfoTest0001)
+{
+	//Open(25);
+	//Sleep(2000);
+	EXPECT_EQ(ELOCK_ERROR_SUCCESS,SetRecvMsgRotine(myATMCRecvMsgRotine));	
+	const JC_MSG_TYPE msgType=JCMSG_SEND_LOCK_ACTIVEINFO;	//设定消息类型
+	//ATMC生成XML消息
+	string strSendLockActInfoXML;	//容纳生成的消息XML
+	//具体生成消息XML
+	ptree pt;
+	jcAtmcMsg::zwAtmcTestMsgGen(msgType,strSendLockActInfoXML, pt);	
+
+	//cout<<"Sleep 65 seconds,please wait"<<endl;
+	//Sleep(5*1000);
+	if (ELOCK_ERROR_SUCCESS==m_connStatus)
+	{
+		EXPECT_EQ(ELOCK_ERROR_SUCCESS,Notify(strSendLockActInfoXML.c_str()));		
+	}
+	else
+	{
+		EXPECT_EQ(ELOCK_ERROR_CONNECTLOST,Notify(strSendLockActInfoXML.c_str()));		
+		cout<<"Server not Start!"<<endl;
+	}
+
+	while (s_repLockInitXML.length()==0)
+	{
+		Sleep(200);
+	}
+	//Sleep(ZW_END_WAIT);
+	EXPECT_LT(42,s_repLockInitXML.length());
+	string ccbop,ccbname;
+	zwGetCCBMsgType(s_repLockInitXML,ccbop,ccbname);
+	EXPECT_EQ("0001",ccbop);
+	EXPECT_EQ("SendActInfo",ccbname);
+
+}
+#endif // _DEBUG_SEND_ACTINFO
+
