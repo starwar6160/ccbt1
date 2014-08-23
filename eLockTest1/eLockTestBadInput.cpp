5c906a67358c127d5b57938e9bb3f68bd329b046
Zhou Wei <zhouwei6160@gmail.com>
2014-08-23 22:05:58 +0800

    gtest中的非法输入，死亡测试提取到单独的代码文件中了

---
+++ eLockTest1/eLockTestBadInput.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "jcElockTestHdr.h"
+#include "CCBelock.h"
+
+#ifdef _ZWTEST730
+//SetRecvMsgRotine测试
+TEST_F(ccbElockTest,SetRecvMsgRotineTestInvalidXML)
+{
+	//Open(25);
+	EXPECT_EQ(ELOCK_ERROR_SUCCESS,SetRecvMsgRotine(myATMCRecvMsgRotine));	
+	if (ELOCK_ERROR_SUCCESS==m_connStatus)
+	{
+		EXPECT_EQ(ELOCK_ERROR_CONNECTLOST,Notify("test20140730.1558forCallBackTESTFORCHAR42AND"));		
+	}
+	else
+	{
+		EXPECT_EQ(ELOCK_ERROR_CONNECTLOST,Notify("test20140730.1558forCallBackTESTFORCHAR42ANDNoConnforCallBack"));		
+		cout<<"Server not Start!"<<endl;
+	}
+
+#ifdef NDEBUG
+	EXPECT_EQ(ELOCK_ERROR_PARAMINVALID,SetRecvMsgRotine(NULL));
+#endif // NDEBUG
+	//Close();
+}
+
+TEST(ccbElockDeathTest,SetRecvMsgRotineTestBad)
+{//Debug下会触发断言的非法输入测试，放到死亡测试中进行
+	EXPECT_DEBUG_DEATH(SetRecvMsgRotine(NULL),"");
+}
+
+TEST(ccbElockDeathTest,OpenTestBad)
+{//Debug下会触发断言的非法输入测试，放到死亡测试中进行
+	EXPECT_DEBUG_DEATH(Open(0),"");
+	EXPECT_DEBUG_DEATH(Open(-30),"");
+	EXPECT_DEBUG_DEATH(Open(90000),"");
+}
+
+
+TEST(ccbElockDeathTest,NotifyTestBad)
+{//Debug下会触发断言的非法输入测试，放到死亡测试中进行
+	//EXPECT_DEBUG_DEATH(Notify(myLongMsg),"");
+	EXPECT_DEBUG_DEATH(Notify(""),"");
+	EXPECT_DEBUG_DEATH(Notify(NULL),"");
+}
+
+#endif // _ZWTEST730
+
--- eLockTest1/jcElockTestHdr.h
+++ eLockTest1/jcElockTestHdr.h
@@ -4,7 +4,7 @@
 #include "zwCcbElockHdr.h"
 using namespace boost::property_tree;
 //看看是否打开其他测试以便专一测试一件事
-//#define _ZWTEST730
+#define _ZWTEST730
 //第一阶段的3条测试是否打开
 //锁具可以正确反应的2个测试
 #define _DEBUG_ACTREQ
