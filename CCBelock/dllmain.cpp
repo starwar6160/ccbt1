b5c7d362a4b334c196e9e66198595a755384192d
Zhou Wei <zhouwei6160@gmail.com>
2014-08-24 20:44:03 +0800

    基本把日志和配置代码挪动到单独代码文件中，但是去掉DLLMAIN中的日志调用，就会导致后面注册回调函数时出现空指针，原因未明

--- CCBelock/dllmain.cpp
+++ CCBelock/dllmain.cpp
@@ -1,10 +1,6 @@
 // dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "stdafx.h"
-#include <direct.h>
-#include <WinBase.h>
-#include "zwCcbElockHdr.h"
 
-string zwGetLogFileName(void);
 void myLoadCfgs();
 extern Poco::LogStream *pocoLog;
 
@@ -35,33 +31,6 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
 
 
-Poco::LogStream *pocoLog=NULL;
-int PocoLogInit(void)  
-{  
-	Poco::AutoPtr<Poco::Channel> channel;  
-	{  
-
-		Poco::AutoPtr<Poco::Channel> fileChannel(new Poco::FileChannel());  
-		fileChannel->setProperty("compress", "true");  
-		fileChannel->setProperty("path", 
-			zwGetLogFileName()); 
-		fileChannel->setProperty("archive", "number");  
-		fileChannel->setProperty("purgeAge", "30 days");  
-		fileChannel->setProperty("rotation", "6 hours");  
-		fileChannel->setProperty("times", "local"); 
-
-		Poco::AutoPtr<Poco::PatternFormatter> patternFormatter(new Poco::PatternFormatter());  
-		patternFormatter->setProperty("pattern", "%H:%M:%S:%i %s(%l): %t");  
-		channel = new Poco::FormattingChannel(patternFormatter, fileChannel);  
-	}  
-
-
-	Poco::Logger::root().setChannel(channel.get());  
-	Poco::Logger& logger = Poco::Logger::get("LockDLL");  
-	logger.setLevel(Poco::Message::PRIO_TRACE);
-	pocoLog=new Poco::LogStream(logger); 
-	return 0;  
-}  
 
 
 
--- CCBelock/jcPocoLogCfg.cpp
+++ CCBelock/jcPocoLogCfg.cpp
@@ -31,10 +31,38 @@ string zwGetLogFileName(void)
 	return jcLogPath+"\\"+logdate+"."+logtime+"JinChuLog.txt";
 }
 
+
+Poco::LogStream *pocoLog=NULL;
+int PocoLogInit(void)  
+{  
+	Poco::AutoPtr<Poco::Channel> channel;  
+	{  
+
+		Poco::AutoPtr<Poco::Channel> fileChannel(new Poco::FileChannel());  
+		fileChannel->setProperty("compress", "true");  
+		fileChannel->setProperty("path", 
+			zwGetLogFileName()); 
+		fileChannel->setProperty("archive", "number");  
+		fileChannel->setProperty("purgeAge", "30 days");  
+		fileChannel->setProperty("rotation", "6 hours");  
+		fileChannel->setProperty("times", "local"); 
+
+		Poco::AutoPtr<Poco::PatternFormatter> patternFormatter(new Poco::PatternFormatter());  
+		patternFormatter->setProperty("pattern", "%H:%M:%S:%i %s(%l): %t");  
+		channel = new Poco::FormattingChannel(patternFormatter, fileChannel);  
+	}  
+
+
+	Poco::Logger::root().setChannel(channel.get());  
+	Poco::Logger& logger = Poco::Logger::get("LockDLL");  
+	logger.setLevel(Poco::Message::PRIO_TRACE);
+	pocoLog=new Poco::LogStream(logger); 
+	return 0;  
+}  
+
 void myLoadCfgs()
 {
 	//在DLL的同一个目录下寻找同名的INI文件载入
 	zwccbthr::myLoadConfig(zwGetConfigFileName());
-
 	PocoLogInit();
 }
