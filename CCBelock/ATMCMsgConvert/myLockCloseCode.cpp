d82422f12e2dc431ec42a9237286a86bb1febd85
Zhou Wei <zhouwei6160@gmail.com>
2014-09-02 14:02:28 +0800

    闭锁码，校验码的几条报文分离单独代码文件了

---
+++ CCBelock/ATMCMsgConvert/myLockCloseCode.cpp
@@ -0,0 +1,97 @@
+#include "stdafx.h"
+#include "zwCcbElockHdr.h"
+#include "myConvIntHdr.h"
+
+namespace jcAtmcConvertDLL{
+	//读取闭锁码
+	void zwconvReadCloseCodeDown( const ptree &, ptree &ptjc )
+	{
+		ZWFUNCTRACE
+			//>> 读取闭锁码
+			//请求
+			//	"command": JCSTR_READ_CLOSECODE,
+			//	"Lock_Time"	
+			//应答
+			//"Command":JCSTR_READ_CLOSECODE
+
+
+			ptjc.put(jcAtmcConvertDLL::JCSTR_CMDTITLE,JCSTR_READ_CLOSECODE);
+		ptjc.put("Lock_Time",time(NULL));
+	}
+
+	void zwconvReadCloseCodeUp( const ptree &ptjc, ptree &ptccb )
+	{
+		ZWFUNCTRACE
+			//读取闭锁码 应答
+			//	交易代码	TransCode	是	值：0004
+			//	交易名称	TransName	是	值：ReadShutLockCode
+			//	交易日期	TransDate	是	值：YYYYMMDD，如20140401
+			//	交易时间	TransTime	是	值：hhmmss，如134050
+			//	ATM设备编号	DevCode	是	值：我行12位设备编号
+			//	锁具厂商	LockMan	是	值：厂商自定与其他厂商不同的名称
+			//	锁具编号	LockId	是	值：厂商自定的锁具唯一编号
+			//	闭锁码	ShutLockcode	是	值：闭锁码
+			//无用的形式化部分
+			ptccb.put(CCBSTR_CODE,"0004");
+		assert("ReadShutLockCode"==ns_ReadCloseCodeName);
+		ptccb.put(CCBSTR_NAME,ns_ReadCloseCodeName);
+		string zwDate,zwTime;
+		zwGetLocalDateTimeString(time(NULL),zwDate,zwTime);
+		ptccb.put(CCBSTR_DATE,zwDate);
+		ptccb.put(CCBSTR_TIME,zwTime);
+
+		ptccb.put(CCBSTR_DEVCODE,ns_ccbAtmno);
+		ptccb.put("root.LockMan",LOCKMAN_NAME);
+		ptccb.put("root.LockId",ptjc.get<string>("Lock_Serial"));
+		ptccb.put("root.ShutLockcode",ptjc.get<int>("Code"));
+	}
+
+	void zwconvRecvInitCloseCodeDown(const ptree &, ptree &ptjc )
+	{
+		ZWFUNCTRACE
+			ptjc.put(jcAtmcConvertDLL::JCSTR_CMDTITLE,"Lock_Close_Code_Lock");
+	}
+
+
+	void zwconvRecvInitCloseCodeUp(const ptree &ptjc, ptree &ptccb)
+	{
+		ZWFUNCTRACE
+			ptccb.put(CCBSTR_CODE,"1000");
+		ptccb.put(CCBSTR_NAME,"SendShutLockCode");
+		string zwDate,zwTime;
+		zwGetLocalDateTimeString(time(NULL),zwDate,zwTime);
+		ptccb.put(CCBSTR_DATE,zwDate);
+		ptccb.put(CCBSTR_TIME,zwTime);
+		//锁具发送初始闭锁码时，ATM编号应该已经在激活请求中获得，但是
+		//1.1版本报文里面没有给出，所以此处可能会有问题
+		ptccb.put(CCBSTR_DEVCODE,ns_ccbAtmno);	
+		ptccb.put("root.LockMan",LOCKMAN_NAME);
+		ptccb.put("root.ShutLockcode",ptjc.get<int>("Code"));
+	}
+
+	void zwconvRecvVerifyCodeDown(const ptree &, ptree &ptjc )
+	{
+		ZWFUNCTRACE
+			ptjc.put(jcAtmcConvertDLL::JCSTR_CMDTITLE,"Lock_Open_Ident");
+	}
+
+	void zwconvRecvVerifyCodeUp(const ptree &ptjc, ptree &ptccb)
+	{
+		ZWFUNCTRACE
+			ptccb.put(CCBSTR_CODE,"1002");
+		ptccb.put(CCBSTR_NAME,"SendUnLockIdent");
+		string zwDate,zwTime;
+		zwGetLocalDateTimeString(time(NULL),zwDate,zwTime);
+		ptccb.put(CCBSTR_DATE,zwDate);
+		ptccb.put(CCBSTR_TIME,zwTime);
+		//锁具发送初始闭锁码时，ATM编号应该已经在激活请求中获得，但是
+		//1.1版本报文里面没有给出，所以此处可能会有问题
+		ptccb.put(CCBSTR_DEVCODE,ns_ccbAtmno);	
+		ptccb.put("root.LockMan",LOCKMAN_NAME);
+		ptccb.put("root.LockId",ptjc.get<string>("Lock_Serial"));
+		//关键的验证码本体
+		ptccb.put("root.UnLockIdentInfo",ptjc.get<int>("Lock_Ident_Info"));
+	}
+
+
+}	//namespace jcAtmcConvertDLL{
