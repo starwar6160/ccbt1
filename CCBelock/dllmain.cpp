ca987465f9eb20d327164de60e0d322a402e4cd7
Zhou Wei <zhouwei6160@gmail.com>
2014-09-05 17:47:50 +0800

    添加了对于zwBaseLibStatic这个静态库中的串口类的引用，编译通过，但尚未测试

--- CCBelock/dllmain.cpp
+++ CCBelock/dllmain.cpp
@@ -1,6 +1,6 @@
 // dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "stdafx.h"
-
+#include "jcSerialPort.h"
 void myLoadCfgs();
 extern Poco::LogStream * pocoLog;
 
@@ -24,4 +24,32 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 		break;
 	}
 	return TRUE;
+}
+
+
+int sptest905a17(void)
+{
+	//	以下是十六进制的串口调试助手发送数据，开头8个字节是大端结尾的数字50(0x32)的HEX表示，后面是5组"0123456789"的HEX表示
+	//	000000323031323334353637383930313233343536373839303132333435363738393031323334353637383930313233343536373839
+
+	jcSerialPort jcsp("COM2");
+	string msg1="ZhouWei20140905.1112\n";
+	jcsp.SendData(msg1.c_str(),msg1.size());
+	const int BLEN=500;
+	char recvBuf[BLEN+1];
+	memset(recvBuf,0,BLEN+1);
+	int outLen=0;
+	Sleep(5000);
+	for (int i=0;i<20;i++)
+	{
+		memset(recvBuf,0,BLEN);
+		jcsp.RecvData(recvBuf, BLEN,&outLen);
+		//cout<<"R "<<recvBuf<<endl;
+		Sleep(500);
+	}
+
+	//Sleep(6000);
+	//jcsp.RecvData(recvBuf,&outLen);
+
+	return 0;
 }
