645bf5e61331a71244bcdecc35e34c0687dcca6f
Zhou Wei <zhouwei6160@gmail.com>
2014-09-10 11:59:39 +0800

    把zwPocoLog.h里面的pocoLog对象引用删除了，程序暂时正常

--- CCBelock/zwPocoLog.h
+++ CCBelock/zwPocoLog.h
@@ -5,11 +5,11 @@
 
 extern Poco::LogStream *pocoLog;
 //#define ZWFATAL(x)	OutputDebugStringA("ZWFATAL"); OutputDebugStringA(x);
-#define ZWDEBUG(x) pocoLog->debug(x);OutputDebugStringA(x);
-#define ZWINFO(x) pocoLog->information(x);OutputDebugStringA(x);
-#define ZWNOTICE(x) pocoLog->notice(x);OutputDebugStringA(x);
-#define ZWWARN(x) pocoLog->warning(x);OutputDebugStringA(x);
-#define ZWERROR(x) pocoLog->error(x);OutputDebugStringA(x);
-#define ZWFATAL(x) pocoLog->fatal(x);OutputDebugStringA(x);
+#define ZWDEBUG(x) OutputDebugStringA(x);
+#define ZWINFO(x) OutputDebugStringA(x);
+#define ZWNOTICE(x) OutputDebugStringA(x);
+#define ZWWARN(x) OutputDebugStringA(x);
+#define ZWERROR(x) OutputDebugStringA(x);
+#define ZWFATAL(x) OutputDebugStringA(x);
 
 #endif // zwPocoLog_h__
