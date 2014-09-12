c682e2fe72195f9e199f1bd9ba1dc0c22eb09b2a
Zhou Wei <zhouwei6160@gmail.com>
2014-09-12 16:00:42 +0800

    简单测试了一下boost lexical_cast的正确性，能否正常使用；

--- CCBelock/dllmain.cpp
+++ CCBelock/dllmain.cpp
@@ -1,14 +1,28 @@
 // dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "stdafx.h"
-
+#include "zwCcbElockHdr.h"
 void myLoadCfgs();
 extern Poco::LogStream * pocoLog;
+
+#ifdef _DEBUG
+string zwTest912(string now)
+{
+	time_t logGenData=boost::lexical_cast<time_t>(now);
+	string exDate, exTime;
+	zwGetLocalDateTimeString(logGenData, exDate, exTime);
+	return exDate+"."+exTime;
+}
+#endif // _DEBUG
+
+
 
 BOOL APIENTRY DllMain(HMODULE hModule,
 		      DWORD ul_reason_for_call, LPVOID lpReserved)
 {
 	myLoadCfgs();
-
+#ifdef _DEBUG
+	cout<<"TEST912 LEXICAST.1557.\t"<<zwTest912("1409023024")<<endl;
+#endif // _DEBUG
 	switch (ul_reason_for_call) {
 	case DLL_PROCESS_ATTACH:
 		pocoLog->warning("JINCHU ELOCK DLL_PROCESS_ATTACH");
