06a4af319dc6880bc7519281e4bcdab44f1919ef
Zhou Wei <zhouwei6160@gmail.com>
2014-09-26 11:51:02 +0800

    zwGetLocalDateTimeString函数挪出，挪到了zwBaseLibStatic里面

--- CCBelock/jcJsonCommandDefine.cpp
+++ CCBelock/jcJsonCommandDefine.cpp
@@ -21,21 +21,4 @@ namespace jcAtmcConvertDLL {
 	const char *CCBSTR_TIME = "root.TransTime";
 	const char *CCBSTR_DEVCODE = "root.DevCode";
 }
-//从给定的时间秒数，获取日期(YYYYMMDD)和时间(HHMMSS)字符串
-    void zwGetLocalDateTimeString(time_t inTime, string & outDate,
-				  string & outTime)
-{
-	char zwDate[16];	//YYYYMMDD+NULL
-	char zwTime[16];	//HHMMSS+NULL
-	memset(zwDate, 0, 16);
-	memset(zwTime, 0, 16);
-	time_t tmpTime = inTime;
-	//assert(tmpTime>1400*1000*1000);
-	struct tm *p;
-	p = localtime(&tmpTime);	//取得当地时间
-	sprintf(zwDate, "%04d%02d%02d", (1900 + p->tm_year), (p->tm_mon + 1),
-		p->tm_mday);
-	sprintf(zwTime, "%02d%02d%02d", p->tm_hour, p->tm_min, p->tm_sec);
-	outDate = zwDate;
-	outTime = zwTime;
-}
+
