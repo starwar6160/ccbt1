ddcdec46cb3ad44779ee0ae10831c612806c676f
Zhou Wei <zhouwei6160@gmail.com>
2014-10-18 14:49:19 +0800

    密盒测试主程序增加了注释

--- elockcstest808/Program.cs
+++ elockcstest808/Program.cs
@@ -27,9 +27,9 @@ namespace elockcstest808
 
 
         static void Main(string[] args)
-        {
-            //myTest1();
+        {            
             Console.Out.WriteLine("Secret Box Open");
+            //打开密盒
             jclms.JC_SECBOX_STATUS status =
                 jclms.HidProtocol.SecboxAuth();
             if(status==JC_SECBOX_STATUS.JC_SECBOX_SUCCESS)
@@ -40,11 +40,17 @@ namespace elockcstest808
             {
                 Console.Out.WriteLine("Fake Secret Box");
             }
+            //如果成功通过了密盒认证，则获取的密盒句柄才是有效的，才能成功用于下面的数据读写
             int hnd = jclms.HidProtocol.SecboxGetHandle();
 //////////////////////////////////////////////////////////
+            //随便用一段比较长的文字经过base64编码形成的下面这段有待写入的base64数据
+            //实践中，可以用二进制数据编码之后成为base64字符串写入；
+            //第二个参数是索引号，大致上是0到10左右，具体还得和赵工确认
+            //第三个参数，也就是数据，大体上可以达到最大400多个字节，具体多少还得和赵工确认
             const String myLongB64Str1 = "emhvdXdlaXRlc3RPdXRwdXREZWJ1Z1N0cmluZ0FuZEppbkNodUVMb2NraW5kZXg9MFRvdGFsQmxvY2s9MkN1ckJsb2NrTGVuPTU4U2VkaW5nIERhdGEgQmxvY2sgIzBSZWNldmVkIERhdGEgRnJvbSBKQ0VMb2NrIGlzOg==";
             Console.Out.WriteLine("Secret Box WriteData");
             jclms.HidProtocol.SecboxWriteData(hnd, 1, myLongB64Str1);
+            //通过句柄，索引号，读取密盒数据，返回的也是Base64编码过的字符串，解码后可能是文本，也可能是二进制数据
             Console.Out.WriteLine("Secret Box ReadData");
             String recvFromSecBox = jclms.HidProtocol.SecboxReadData(hnd, 1);
         }
