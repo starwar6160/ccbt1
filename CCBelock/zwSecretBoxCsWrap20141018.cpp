e6931c095c58082b899de928d79bc3cf88732a26
Zhou Wei <zhouwei6160@gmail.com>
2014-10-22 11:31:49 +0800

    暂时添加了一些日志，在密盒C#接口这个层次，看看有没有问题

--- CCBelock/zwSecretBoxCsWrap20141018.cpp
+++ CCBelock/zwSecretBoxCsWrap20141018.cpp
@@ -1,11 +1,13 @@
 //#include "CCBelock.h"
+#include "stdafx.h"
 #include "zwSecretBoxCCBcsWrap.h"
 #include "zwSecBoxCSHdr.h"
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
 #include <windows.h>
-#define PRFN	printf("%s\n",__FUNCTION__);OutputDebugStringA(__FUNCTION__);
+extern Poco::LogStream * pocoLog;
+#define PRFN	OutputDebugStringA(__FUNCTION__);pocoLog->information()<<__FUNCTION__<<endl;printf("%s\n",__FUNCTION__);
 
 static int g_hidHandle;	//单例模式密盒HID句柄，new多少个类都是这个全局变量保存HID通信句柄，等于一个类；
 
@@ -36,6 +38,7 @@ JcSecBox::JcSecBox()
 {
 	PRFN	
 		if(NULL==g_hidHandle){
+			pocoLog->information("jcSecBox OpenFirst");
 			g_hidHandle=zwSecboxHidOpen();
 		}		
 		assert(NULL!=g_hidHandle);
@@ -43,6 +46,7 @@ JcSecBox::JcSecBox()
 		{
 			OutputDebugStringA("JcSecBox open FAIL");
 			printf("%s OPEN HID JINCHU SECRET BOX FAIL!\n");
+			pocoLog->error("jcSecBox OpenNULL");
 		}
 }
 
@@ -51,6 +55,7 @@ JcSecBox::~JcSecBox()
 	PRFN
 		if (NULL!=g_hidHandle)
 		{
+			pocoLog->information()<<"jcSecBox Closed handle="<<g_hidHandle<<endl;
 			zwSecboxHidClose(g_hidHandle);
 		}	
 }
@@ -59,23 +64,28 @@ JcSecBox::~JcSecBox()
 
 int JcSecBox::SecboxAuth( void )
 {
-	PRFN
+	PRFN	
 	if (0==g_hidHandle)
 	{
+		pocoLog->error("jcSecbox Error HandleZero");
 		return JC_SECBOX_FAIL ;
 	}
 	printf("*****************************SecretBox zwSendAuthReq2SecBox\n");
+	pocoLog->information()<<"zwSendAuthReq2SecBox"<<g_hidHandle<<endl;
 	zwSendAuthReq2SecBox(g_hidHandle);
 	printf("*****************************SecretBox zwVerifyAuthRspFromSecBox\n");
+	pocoLog->information()<<"zwVerifyAuthRspFromSecBox"<<g_hidHandle<<endl;
 	int AuthRes=zwVerifyAuthRspFromSecBox(g_hidHandle);
 	
 	if (0==AuthRes)
 	{
+		pocoLog->information()<<"SecboxAuth SUCCESSJC"<<endl;
 		printf("1021.1355.****************************************** *SecretBox Auth SUCCESS\n");
 		return JC_SECBOX_SUCCESS;
 	}
 	else
 	{
+		pocoLog->error()<<"SecboxAuth FAILJC"<<endl;
 		OutputDebugStringA("JcSecBox AUTH FAIL###########################");
 		printf("************************************************** ***SecretBox Auth FAIL\n");
 		return JC_SECBOX_FAIL;
@@ -87,24 +97,32 @@ int JcSecBox::SecboxWriteData( const int index,const char *dataB64 )
 	PRFN
 	if (0==g_hidHandle)
 	{
+		pocoLog->error("jcSecbox Error HandleZero");
 		return 1;
 	}
 	assert(index>=0 && index<=16);
 	assert(NULL!=dataB64 && strlen(dataB64)>0);
 	if (index<0 || index>16)
 	{
+		pocoLog->error("jcSecbox Error IndexOutofRange");
 		printf("Data Index out of range! must in 0 to 16\n");
 		return 1;
 	}
 	if (NULL==dataB64 || strlen(dataB64)==0)
 	{
+		pocoLog->error("jcSecbox Error InputDataZero");
 		printf("input must base64 encoded string!\n");
 		return 1;
 	}
+	pocoLog->information()<<__FUNCTION__<<"index="<<index<<" input Data="<<dataB64<<endl;
+	pocoLog->information("WriteData Auth Start");
 	int status=SecboxAuth();
+	pocoLog->information("WriteData Auth End");
 	if (JC_SECBOX_SUCCESS==status)
 	{
+		pocoLog->information()<<"WriteData Authed,start write"<<endl;
 		zwWriteData2SecretBox(g_hidHandle,index,dataB64);		
+		pocoLog->information()<<"WriteData end"<<endl;
 	}	
 	return 0;
 }
@@ -115,19 +133,26 @@ const char * JcSecBox::SecboxReadData( const int index )
 	const char *retStr="";
 	if (0==g_hidHandle)
 	{
+		pocoLog->error("jcSecbox Error HandleZero");
 		return retStr;
 	}
 	assert(index>=0 && index<=16);
 	if (index<0 || index>16)
 	{
+		pocoLog->error("jcSecbox Error IndexOutofRange");
 		printf("Data Index out of range! must in 0 to 16\n");
 		return retStr;
 	}
-	
+	pocoLog->information()<<__FUNCTION__<<" index="<<index<<endl;
+	pocoLog->information("ReadData Auth Start");
 	int status=SecboxAuth();
+	pocoLog->information("ReadData Auth End");
 	if (JC_SECBOX_SUCCESS==status)
 	{
+		pocoLog->information()<<"ReadData Authed,start read"<<endl;
 		retStr=zwReadDataFromSecretBox(g_hidHandle,index);
+		pocoLog->information()<<"ReadData end"<<endl;
 	}	
+	pocoLog->information()<<__FUNCTION__<<"Return "<<retStr<<endl;
 	return retStr;
 }
