07e0262ddd36f2d47916f1d34fc2b950a48e939f
Zhou Wei <zhouwei6160@gmail.com>
2015-01-08 09:22:53 +0800

    设置代码页的方法后来证明无法解决PocoLog在中文路径下的问题，所以做了一个异常捕获以及提示。

--- CCBelock/dllmain.cpp
+++ CCBelock/dllmain.cpp
@@ -18,41 +18,50 @@ string zwTest912(string now)
 
 BOOL APIENTRY DllMain(HMODULE hModule,
 		      DWORD ul_reason_for_call, LPVOID lpReserved)
-{
-	//设置代码页为简体中文，936是简体中文的代码页。
-	std::locale loc1 = std::locale::global(std::locale(".936"));
-
-	OutputDebugStringA("JinChu Elock DllMain Loaded.20141017.1023");
-	G_DLL_HMODULE = hModule;	//方便其他需要获取DLL绝对路径的地方使用；
-	char myDllPath[256];
-	memset(myDllPath, 0, 256);
-	zwGetDLLPath(hModule, myDllPath, 256);
-	OutputDebugStringA(myDllPath);
-	myLoadCfgs(myDllPath);
+{//
+	try
+	{ 
+		//设置代码页为简体中文，936是简体中文的代码页。
+		std::locale loc1 = std::locale::global(std::locale(".936"));
+		OutputDebugStringA("JinChu Elock DllMain Loaded.20141017.1023");
+		G_DLL_HMODULE = hModule;	//方便其他需要获取DLL绝对路径的地方使用；
+		char myDllPath[256];
+		memset(myDllPath, 0, 256);
+		zwGetDLLPath(hModule, myDllPath, 256);
+		OutputDebugStringA(myDllPath);
+		myLoadCfgs(myDllPath);
 #ifdef _DEBUG
-	//cout<<"TEST912 LEXICAST.1557.\t"<<zwTest912("1409023024")<<endl;
+		//cout<<"TEST912 LEXICAST.1557.\t"<<zwTest912("1409023024")<<endl;
 #endif // _DEBUG
-	switch (ul_reason_for_call) {
-	case DLL_PROCESS_ATTACH:
-		OutputDebugStringA("JINCHU ELOCK DLL_PROCESS_ATTACH");
-		pocoLog->warning("JINCHU ELOCK DLL_PROCESS_ATTACH");
-		break;
-	case DLL_THREAD_ATTACH:
-		OutputDebugStringA("JINCHU ELOCK DLL_THREAD_ATTACH");
-		pocoLog->notice("JINCHU ELOCK DLL_THREAD_ATTACH");
-		break;
-	case DLL_THREAD_DETACH:
-		OutputDebugStringA("JINCHU ELOCK DLL_THREAD_DETACH");
-		pocoLog->notice("JINCHU ELOCK DLL_THREAD_DETACH");
-		break;
-	case DLL_PROCESS_DETACH:
-		OutputDebugStringA("JINCHU ELOCK DLL_PROCESS_DETACH");
-		pocoLog->warning("JINCHU ELOCK DLL_PROCESS_DETACH");
-		break;
+		switch (ul_reason_for_call) {
+		case DLL_PROCESS_ATTACH:
+			OutputDebugStringA("JINCHU ELOCK DLL_PROCESS_ATTACH");
+			pocoLog->warning("JINCHU ELOCK DLL_PROCESS_ATTACH");
+			break;
+		case DLL_THREAD_ATTACH:
+			OutputDebugStringA("JINCHU ELOCK DLL_THREAD_ATTACH");
+			pocoLog->notice("JINCHU ELOCK DLL_THREAD_ATTACH");
+			break;
+		case DLL_THREAD_DETACH:
+			OutputDebugStringA("JINCHU ELOCK DLL_THREAD_DETACH");
+			pocoLog->notice("JINCHU ELOCK DLL_THREAD_DETACH");
+			break;
+		case DLL_PROCESS_DETACH:
+			OutputDebugStringA("JINCHU ELOCK DLL_PROCESS_DETACH");
+			pocoLog->warning("JINCHU ELOCK DLL_PROCESS_DETACH");
+			break;
+		}
+		OutputDebugStringA("JinChu Elock DllMain Success End.20141017.1023");
+		//恢复原来的代码页
+		std::locale::global(std::locale(loc1));
 	}
-	OutputDebugStringA("JinChu Elock DllMain Success End.20141017.1023");
-	//恢复原来的代码页
-	std::locale::global(std::locale(loc1));
+	catch(...)
+	{
+		//为了获取中文目录下Poco的日志库出异常时的问题
+		printf("PocoLog in Chinese PathName Execption!20150108\n");
+		printf("请把本ATMC DLL放到纯英文，无空格的路径下才能正确被调用，谢谢！Zhou Wei.20150108.\n")
+		exit(2015);
+	}
 
 	return TRUE;
 }
