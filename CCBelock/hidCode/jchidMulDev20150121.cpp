0f0b24fcdc0e14ecd02ac73eb435533ad2aaa457
zhouwei6160 <zhouwei6160@gmail.com>
2015-01-21 16:24:50 +0800

    step1624.通过关闭设备之前，使用较长的5秒延时，所有的回复消息都收到了，并且是正确的；

--- CCBelock/hidCode/jchidMulDev20150121.cpp
+++ CCBelock/hidCode/jchidMulDev20150121.cpp
@@ -152,7 +152,7 @@ const char * zwJcHidDbg15A::RecvFromLockJsonThr(void)
 		uint32_t recvDataSum=0;
 		int t_thr_runCount=1;
 		while (1) {	
-			LOG(WARNING)<<"RECV THR "<<t_thr_runCount++<<endl;
+			//LOG(WARNING)<<"RECV THR "<<t_thr_runCount++<<endl;
 			/** 手动在线程中加入中断点，中断点不影响其他语句执行 */  
 			boost::this_thread::interruption_point();  
 					if (NULL==m_dev.hid_device)
