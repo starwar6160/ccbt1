4f7d778b986150776ec824a08c55b601c5c2e166
zhouwei6160 <zhouwei6160@gmail.com>
2015-01-30 09:27:41 +0800

    通过复制类声明到gtest代码文件中，并在开头include需要的自定义类型的头文件，成功在gtest代码中实例化hid设备类对象

--- CCBelock/hidCode/zwHidMulTest20150128.cpp
+++ CCBelock/hidCode/zwHidMulTest20150128.cpp
@@ -2,6 +2,33 @@
 #include <gtest/gtest.h>
 #define ZWUSEGTEST
 #include "CCBelock.h"
+#include "zwHidMulHeader.h"
+#include "zwHidComm.h"
+
+namespace jcLockJsonCmd_t2015a21{
+	class zwJcHidDbg15A
+	{
+	public:
+		zwJcHidDbg15A();	
+		~zwJcHidDbg15A();
+		//序列号如果为NULL则不使用序列号打开设备
+		int OpenElock(const char *ElockSerial);
+		int OpenSecBox(const char *SecBoxSerial);
+		uint32_t PushJson(const char *strJsonCmd);		
+		uint32_t GetHash(void);
+	private:
+		JCHID m_dev;
+		uint32_t m_hashId;	//由设备PID字符串("Lock"等)和序列号HASH出来的ID
+		boost::thread *thr;
+		boost::mutex jcSend_mutex;	//用来限定先要打开设备，启动数据接收线程，然后才能发送数据
+		int RecvThread(JCHID *hidHandle);
+		void StartRecvThread();
+		void StopRecvThread();
+		void OpenHidDevice();
+	};
+
+}	//namespace jcLockJsonCmd_t2015a21{
+
 
 #ifdef ZWUSEGTEST
 //测试套件初始化和结束事件
@@ -39,5 +66,8 @@ TEST_F(ATMCDLLSelfTest, LockActiveTest0000)
 {
 	int age=1;
 	EXPECT_EQ(1,age);
+	jch::zwJcHidDbg15A hid1;
+	hid1.OpenElock(NULL);
+	EXPECT_GT(hid1.GetHash(),0);
 }
 #endif // ZWUSEGTEST
